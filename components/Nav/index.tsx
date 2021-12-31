f81e299ca53101d6ccbc0ad0abfbde87f5c46f00
Marius Florescu <mariusflorescu23@icloud.com>
2021-12-31 15:48:06 +0200

    updated header

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,19 +1,45 @@
+import React from "react";
+import { useRouter } from "next/router";
 import Item from "./Item";
 
+type TRoute = {
+  name: string;
+  href: string;
+};
+
 const Nav = () => {
+  const router = useRouter();
+  const title = router.pathname?.split("/")[1];
+
   return (
-    <ul className="list-none py-4 w-full flex justify-end items-center space-x-6 font-semibold">
-      <Item href="/" passHref>
-        home
-      </Item>
-      <Item href="/blog" passHref>
-        blog
-      </Item>
-      <Item href="/contact" passHref>
-        contact
-      </Item>
-    </ul>
+    <nav className="w-full py-6 flex justify-between items-center">
+      <span className="text-neutral-500 dark:text-neutral-400 font-merri text-sm">
+        {title === "" ? "home" : title}
+      </span>
+      <ul className="list-none flex justify-end items-center space-x-6 font-semibold">
+        {routes.map((route: TRoute) => (
+          <Item key={route.name} href={route.href} passHref>
+            {route.name}
+          </Item>
+        ))}
+      </ul>
+    </nav>
   );
 };
+
+const routes: TRoute[] = [
+  {
+    name: "home",
+    href: "/",
+  },
+  {
+    name: "writings",
+    href: "/writings",
+  },
+  {
+    name: "contact",
+    href: "/contact",
+  },
+];
 
 export default Nav;
---
+++ pages/writings.tsx
@@ -0,0 +1,7 @@
+import Layout from "../layout/Main";
+
+const Writings = () => {
+  return <Layout>Hello</Layout>;
+};
+
+export default Writings;
