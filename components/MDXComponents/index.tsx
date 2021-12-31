df4c10cfc9c4bc98abedd41150db5f01d289a797
Marius Florescu <mariusflorescu23@icloud.com>
2021-12-31 21:08:21 +0200

    added context menu with copy to clipboard

--- components/MDXComponents/index.tsx
+++ components/MDXComponents/index.tsx
@@ -1,4 +1,27 @@
 import React from "react";
+import ContextMenu from "@components/ContextMenu";
+
+const codeBlock = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLPreElement>,
+    HTMLPreElement
+  >
+) => {
+  const ref = React.useRef<HTMLPreElement>(null);
+  const [text, setText] = React.useState("");
+
+  React.useEffect(() => {
+    if (ref && ref.current) {
+      setText(ref.current.innerText);
+    }
+  }, []);
+
+  return (
+    <ContextMenu textToCopy={text}>
+      <pre ref={ref} {...props} />
+    </ContextMenu>
+  );
+};
 
 const inlineCode = (
   props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
@@ -10,6 +33,7 @@ const inlineCode = (
 );
 
 const MDXComponents = {
+  pre: codeBlock,
   inlineCode,
 };
 
