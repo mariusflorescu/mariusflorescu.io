2fc2e24b333863e6e854c2df5592beaa2a7b6aa3
Marius Florescu <mariusflorescu23@icloud.com>
2021-12-31 20:41:48 +0200

    select all on inline code

--- components/MDXComponents/index.tsx
+++ components/MDXComponents/index.tsx
@@ -4,7 +4,7 @@ const inlineCode = (
   props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
 ) => (
   <code
-    className="before:content-[''] after:content-[''] px-1 bg-neutral-200 dark:bg-neutral-800 rounded"
+    className="before:content-[''] after:content-[''] px-1 bg-neutral-200 dark:bg-neutral-800 rounded select-all"
     {...props}
   ></code>
 );
