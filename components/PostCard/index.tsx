3a182ab965e2afb1fb6ac8860b79c573445febe6
Marius Florescu <mariusflorescu23@icloud.com>
2022-01-01 21:44:29 +0200

    styled post card on hover

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -15,7 +15,7 @@ type TProps = {
 const PostCard: React.FC<TProps> = ({ post }) => {
   return (
     <Link href={`/writing/${post.slug}`}>
-      <div className="cursor-pointer w-full flex items-center space-x-2 divide-x divide-neutral-200 dark:divide-neutral-800">
+      <div className="group py-4 cursor-pointer w-full rounded-md flex items-center space-x-2 divide-x divide-neutral-200 dark:divide-neutral-800 transition-colors duration-200 hover:bg-neutral-200 hover:dark:bg-neutral-800 hover:divide-neutral-400 hover:dark:divide-neutral-700">
         <em className="mx-4 text-neutral-600 dark:text-neutral-400 text-sm">
           {post.publishedAt}
         </em>
