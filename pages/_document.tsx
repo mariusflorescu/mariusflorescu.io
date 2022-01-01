f9caeebf53cbf1d4480e1e3ded06c3e80ba2e3c0
Marius Florescu <mariusflorescu23@icloud.com>
2022-01-01 19:14:39 +0200

    removed unused font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Playfair+Display:ital,wght@1,400;1,500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Playfair+Display:ital,wght@1,500&display=swap"
             rel="stylesheet"
           />
         </Head>
