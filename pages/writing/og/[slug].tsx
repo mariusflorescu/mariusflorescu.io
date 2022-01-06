402ef305da813c2a08d3dd6f6a2829fbb93f46e9
Marius Florescu <mariusflorescu23@icloud.com>
2022-01-06 21:20:49 +0200

    resized

--- pages/writing/og/[slug].tsx
+++ pages/writing/og/[slug].tsx
@@ -20,11 +20,11 @@ const OgPost: NextPageWithLayout<TProps> = ({ frontMatter }) => {
   return (
     <div className="w-full h-full px-24  bg-gray-1000 text-gray-100 bg-grid-dark font-inter flex flex-col justify-center items-center">
       <Image src="/logoM.png" width={256} height={256} />
-      <h1 className="text-5xl font-bold pb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
+      <h1 className="text-7xl font-bold pb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
         {frontMatter.title}
       </h1>
-      <h4 className="text-xl pb-4">{frontMatter.description}</h4>
-      <h6 className="text-lg font-mono text-gray-500">mariusflorescu.io</h6>
+      <h4 className="text-2xl pb-4">{frontMatter.description}</h4>
+      <h6 className="text-xl font-mono text-gray-400">mariusflorescu.io</h6>
     </div>
   );
 };
