409563eb55941bd20e9dd48a7957a5416049b76f
Marius Florescu <mariusflorescu23@icloud.com>
2022-01-07 00:13:34 +0200

    resized once again lol

--- pages/og/index.tsx
+++ pages/og/index.tsx
@@ -9,11 +9,11 @@ const OgImage: NextPage = () => {
   return (
     <div className="w-full h-full px-24  bg-gray-1000 text-gray-100 bg-grid-dark font-inter flex flex-col justify-center items-center">
       <Image src="/logoM.png" width={256} height={256} />
-      <h1 className="text-8xl font-bold pb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
+      <h1 className="text-7xl font-bold pb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
         {title}
       </h1>
-      <h4 className="text-3xl pb-4">{description}</h4>
-      <h6 className="text-2xl font-mono text-gray-400">mariusflorescu.io</h6>
+      <h4 className="text-2xl pb-4">{description}</h4>
+      <h6 className="text-xl font-mono text-gray-400">mariusflorescu.io</h6>
     </div>
   );
 };
